8338a1da994207f6e0923b5f4f2dd891938bdfc8
yoomarvin <marvin.messenzehl@gmail.com>
2018-11-21 16:59:22 +0100

    wish to router

---
+++ src/components/pages/Wish.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <v-toolbar app>
+      <v-toolbar-title>Wunsch 🎁</v-toolbar-title>
+  </v-toolbar>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">6</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+  </div>
+</template>
+<script>
+export default {
+  name: "Wish"
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,27 +1,29 @@
 // @flow
 
-import '@babel/polyfill';
-import Vue from 'vue';
-import './plugins/vuetify';
-import injector from 'vue-inject';
-import VueRouter from 'vue-router';
-import App from './App.vue'
-import './pipes/currency';
-import Dashboard from './components/pages/Dashboard.vue';
-import { DATA } from './models';
+import "@babel/polyfill";
+import Vue from "vue";
+import "./plugins/vuetify";
+import injector from "vue-inject";
+import VueRouter from "vue-router";
+import App from "./App.vue";
+import "./pipes/currency";
+import Dashboard from "./components/pages/Dashboard.vue";
+import Wish from "./components/pages/Wish.vue";
+import { DATA } from "./models";
 
 Vue.config.productionTip = false;
 
 Vue.use(injector);
 Vue.use(VueRouter);
-Vue.use(require('vue-currency-filter'));
-Vue.use(require('vue-moment'));
+Vue.use(require("vue-currency-filter"));
+Vue.use(require("vue-moment"));
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: __dirname,
   routes: [
-    { path: '/', component: Dashboard }
+    { path: "/", component: Dashboard },
+    { path: "/wish", component: Wish }
   ]
 });
 
@@ -29,5 +31,5 @@ console.log(DATA);
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
