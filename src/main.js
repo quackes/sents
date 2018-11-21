19f8cb45d8d04e0008a92119b66d7dec7c8b9040
Tobias Wingerter <tobias.wingerter@provinzial.com>
2018-11-21 22:35:29 +0100

    no routing mode history

--- src/main.js
+++ src/main.js
@@ -26,7 +26,6 @@ Vue.use(require('vue-currency-filter'));
 Vue.use(require('vue-moment'));
 
 const router = new VueRouter({
-  mode: 'history',
   base: __dirname,
   routes: [
     { path: "/", component: Welcome },
